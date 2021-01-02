0ce19609cd180b0af1461626ba96e4b2c1256948
Konstantin Tenman <ktenman@users.noreply.github.com>
2021-01-02 04:03:13 +0000

    Update Spring and OpenFeign library

--- src/main/java/ee/tenman/investing/InvestingApplication.java
+++ src/main/java/ee/tenman/investing/InvestingApplication.java
@@ -2,12 +2,14 @@ package ee.tenman.investing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
+@EnableFeignClients
 public class InvestingApplication {
 
 	public static void main(String[] args) {
