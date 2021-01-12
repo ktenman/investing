a585c981442f16209d8ff36cf3b3d6d4f11c51c2
Konstantin Tenman <ktenman@users.noreply.github.com>
2021-01-12 02:57:44 +0000

    Update crypto every 3 minutes

--- src/main/java/ee/tenman/investing/service/GoogleService.java
+++ src/main/java/ee/tenman/investing/service/GoogleService.java
@@ -156,7 +156,7 @@ public class GoogleService {
         }
     }
 
-    @Scheduled(cron = "0 29 * * * *")
+    @Scheduled(cron = "25 0/3 * * * *")
     @Retryable(value = {Exception.class}, maxAttempts = 2, backoff = @Backoff(delay = 300))
     public void refreshCryptoPrices() throws Exception {
 
