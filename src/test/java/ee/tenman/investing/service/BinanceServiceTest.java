07d6ce541f39ecf4524824f56ff36d851d02db2b
Konstantin Tenman <ktenman@users.noreply.github.com>
2021-02-06 15:05:43 +0000

    Fix integration test

--- src/test/java/ee/tenman/investing/service/BinanceServiceTest.java
+++ src/test/java/ee/tenman/investing/service/BinanceServiceTest.java
@@ -1,6 +1,7 @@
 package ee.tenman.investing.service;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,6 +11,7 @@ import javax.annotation.Resource;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 @SpringBootTest
 class BinanceServiceTest {
@@ -26,7 +28,6 @@ class BinanceServiceTest {
     @ParameterizedTest
     @ValueSource(strings = {
             "BNB",
-            "CRO",
             "DOT",
             "UNI",
             "BTC",
@@ -41,4 +42,14 @@ class BinanceServiceTest {
 
         assertThat(priceInEuros).isGreaterThan(BigDecimal.ZERO);
     }
+
+    @Test
+    @DisplayName("CROEUR is not supported in Binance")
+    void getPrice() {
+        Throwable thrown = catchThrowable(() -> binanceService.getPriceToEur("CRO"));
+
+        assertThat(thrown.getMessage()).isEqualTo("CROEUR not supported");
+    }
+
+
 }
