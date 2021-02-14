7e736203648e86dde784ffba99e5a5bbf3972742
Konstantin Tenman <ktenman@users.noreply.github.com>
2021-02-14 15:16:59 +0000

    Use path variable instead of request param

--- src/main/java/ee/tenman/investing/integration/yieldwatchnet/api/YieldApiClient.java
+++ src/main/java/ee/tenman/investing/integration/yieldwatchnet/api/YieldApiClient.java
@@ -2,6 +2,7 @@ package ee.tenman.investing.integration.yieldwatchnet.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "yieldApiClient", url = "https://yieldwatch.net/api/")
@@ -9,7 +10,7 @@ public interface YieldApiClient {
 
     @GetMapping(value = "/all/{walletAddress}", produces = "application/json")
     YieldData fetchYieldData(
-            @RequestParam("walletAddress") String walletAddress,
+            @PathVariable("walletAddress") String walletAddress,
             @RequestParam("platforms") String... platforms
     );
 }
